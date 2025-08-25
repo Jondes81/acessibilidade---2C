e72757cefc22d1fbd2eb7fcf5e0c64c52301590b
Jondes81 <164433517+Jondes81@users.noreply.github.com>
2025-08-25 07:11:57 -0300

    a

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', function()){
+document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
 
-        let tamanhoAtualFonte = 1;
+    let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanmhoAtualFonte}rem`
-    
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
-    diminuirFonteBotao.addEventListener('click', function(){
+     diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanmhoAtualFonte}rem`
-    
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
-}//falta parenteses?
+})
